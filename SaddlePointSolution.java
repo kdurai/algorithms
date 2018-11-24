44af9174ce77c9235ed498c7e07808c105ba622e
Durai <durai.kasinathan@gmail.com>
2018-11-24 19:10:54 +0530

    Create SaddlePointSolution.java
    This is to find out number of saddle points in a M X N array. This can be improved further

---
+++ SaddlePointSolution.java
@@ -0,0 +1,59 @@
+package algorithms
+
+public class SaddlePointSolution {
+
+	public int solution(int[][] A)
+	{
+		int saddle = 0;
+		
+		for(int i = 1; i < A.length-1;i++)
+		{
+			for(int j = 1; j < A[i].length-1; j++)
+			{
+				if(A[i][j] < A[i][j-1] && A[i][j] < A[i][j+1])
+				{
+					if(A[i][j] > A[i-1][j] && A[i][j] > A[i+1][j])
+					{
+						++saddle;
+					}
+				}
+				else
+				{
+					if(A[i][j] > A[i][j-1] && A[i][j] > A[i][j+1])
+					{
+						if(A[i][j] < A[i-1][j] && A[i][j] < A[i+1][j])
+						{
+							++saddle;
+						}
+					}
+				}
+					
+			}
+			System.out.println();
+		}
+		return saddle;
+	}
+	public static void main(String[] args) {
+
+		int[][] A = {
+				{0,1,9,3},
+				{7,5,8,3},
+				{9,2,9,4},
+				{4,6,7,1}
+		};
+
+		int[][] B = {
+				{0,1,9,3},
+				{7,5,8,3},
+				{9,2,9,4},
+				{4,6,7,1}
+		};
+		
+		SaddlePointSolution soln = new SaddlePointSolution();
+		System.out.println(soln.solution(A));
+		
+/*		System.out.println(A[0].length);
+		System.out.println(A.length);
+*/	}
+
+}
